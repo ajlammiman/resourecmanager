5f3af6df5d437e5b26d7365cb5ab7588f3f9dffc
Adam Lammiman <adam@johhenry.net>
2017-07-13 21:21:30 +0100

    resource manager setup files

---
+++ src/components/dashboard.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+export default class Dashboard extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            docs : [
+            {
+                key : 1,
+                docTitle : 'document 1',
+                docLink : 'http://www.google.co.uk'
+            },
+            {
+                key : 2,
+                docTitle : 'document 2',
+                docLink : 'http://www.google.co.uk'
+            },
+            {
+                key : 3,
+                docTitle : 'document 3',
+                docLink : 'http://www.google.co.uk'
+            }
+            ]
+        }
+    }
+    
+    render()
+    {
+        console.log(this.state.docs);
+        return(
+        <div>
+            <h2>Document Dashboard</h2>
+            <ul>
+                {this.state.docs.map(function(doc, index){
+                    return <DocumentItem DocTitle={doc.docTitle} DocLink={doc.docLink} />;
+                  })}
+            </ul>
+        </div>)
+    }
+};
+
+const DocumentItem = (props) => {
+    return(
+        <li><a href="{props.DocLink}">{props.DocTitle}</a></li>
+    )
+ };
